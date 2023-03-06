dd8aee8cfcf8068e8ea4a30a35438f4b43032b61
Kyle <2604461c@student.gla.ac.uk>
2023-03-06 12:04:25 +0000

    changes to the settings pages

--- Geko/Screens/RunningScreen.js
+++ Geko/Screens/RunningScreen.js
@@ -1,39 +1,161 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, KeyboardAvoidingView  } from 'react-native';
+import { Image, Alert, Button, Dimensions, StyleSheet, Text, TextInput, View, TouchableOpacity, KeyboardAvoidingView  } from 'react-native';
+import * as Location from 'expo-location';
+import React, {useState, useEffect, Component} from 'react';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
 
 const RunningScreen = ({navigation}) => {
+
+  const [location , setLocation]= useState(null);
+const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+  'Wait, we are fetching you location...'
+);
+
+useEffect( () => {
+  CheckIfLocationEnabled();
+  GetCurrentLocation();
+},[]);
+const GetCurrentLocation = async () => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+
+  if (status !== 'granted') {
+    Alert.alert(
+      'Permission not granted',
+      'Allow the app to use location service.',
+      [{ text: 'OK' }],
+      { cancelable: false }
+    );
+  }
+
+  let { coords } = await Location.getCurrentPositionAsync();
+  
+  if (coords) {
+    const { latitude, longitude } = coords;
+    setLocation(coords);
+    let response = await Location.reverseGeocodeAsync({
+      latitude,
+      longitude
+    });
+
+    for (let item of response) {
+      let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.subregion}`;
+
+      setDisplayCurrentAddress(address);
+    }
+  }
+};
+
+const CheckIfLocationEnabled = async () => {
+  let enabled = await Location.hasServicesEnabledAsync();
+
+  if (!enabled) {
+    Alert.alert(
+      'Location Service not enabled',
+      'Please enable your location services to continue',
+      [{ text: 'OK' }],
+      { cancelable: false }
+    );
+  } else {
+    setLocationServiceEnabled(enabled);
+  }
+};
+
+
   return (
-    <KeyboardAvoidingView
-    style={styles.container}
-    >
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.titlesContainer}>
-          <Text style={styles.titles}>Running Screen!</Text>
+        <Text style={styles.titles}>Running Screen!</Text>
       </View>
-     
-      <View style={styles.buttonContainer}>
-      <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("AR")}
+      <MapView 
+      showsMyLocationButton= {true}
+      showsUserLocation= {true}
+      followsUserLocation= {true}
+      style={styles.map}
+      initialRegion= {{
+        latitude: destination.latitude,
+        longitude: destination.longitude,
+        latitudeDelta: 0.027,
+        longitudeDelta: 0.027,
+      }}
       >
-        <Text style={styles.buttonText}>AR view</Text>
-      </TouchableOpacity>
-      </View>
-     
-     
+      <MapViewDirections
+        origin= {location}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={3}
+        strokeColor="hotpink"
+        onStart={(params) => {
+          console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+        }}
+        onReady={result => {
+          console.log(`Distance: ${result.distance} km`)
+          console.log(`Duration: ${result.duration} min.`)
+          }}
+        onError={(errorMessage) => {
+          console.log('GOT AN ERROR');
+        }
+      }
+      />
+      </MapView>
+      <Text style={styles.text}>{displayCurrentAddress}, </Text>
       <View style={styles.buttonContainer}>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("AR")}>
+          <Text style={styles.buttonText}>AR view</Text>
+          </TouchableOpacity>
+      </View>
+
       <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.navigate("Home")}
       >
         <Text style={styles.buttonText}>End Route</Text>
       </TouchableOpacity>
+      </View> 
 
-      </View>
+      <Button onPress={() => Alert.alert('Simple Button pressed')} 
+      title="OK!" color="#fffff"/>
+  </KeyboardAvoidingView>
+    // <KeyboardAvoidingView
+    // style={styles.container}
+    // >
+    //   <View style={styles.titlesContainer}>
+    //       <Text style={styles.titles}>Running Screen!</Text>
+    //   </View>
+     
+    //   <View style={styles.buttonContainer}>
+    //   <TouchableOpacity
+    //       style={styles.button}
+    //       onPress={() => navigation.navigate("AR")}
+    //   >
+    //     <Text style={styles.buttonText}>AR view</Text>
+    //   </TouchableOpacity>
+    //   </View>
+     
+     
+      // <View style={styles.buttonContainer}>
+      // <TouchableOpacity
+      //     style={styles.button}
+      //     onPress={() => navigation.navigate("Home")}
+      // >
+      //   <Text style={styles.buttonText}>End Route</Text>
+      // </TouchableOpacity>
 
-    </KeyboardAvoidingView>
+    //   </View>
+
+    // </KeyboardAvoidingView>
   );
 }
+
+const origin = {};
+const destination = {latitude:55.841200, longitude:-4.289660 };
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCjjPalVfm8ZkkvuBJvuWbgEDjELOAErXA';
 
 const styles = StyleSheet.create({
   container: {
@@ -41,6 +163,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#1e1e1e',
   },
   button: {
     marginTop: 20,
@@ -67,6 +190,15 @@ const styles = StyleSheet.create({
   titles:{
     color: 'grey',
     fontSize: 30,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '400',
+    color: '#fff'
+  },
+  map: {
+    width: Dimensions.get('window').width/1.1,
+    height: Dimensions.get('window').height/1.5,
   },
 });
 
